2adf2a00463e571a1311a9daed92ab040068f2cd
Rutu <123353444+rutudobariya@users.noreply.github.com>
2024-03-07 12:15:40 +0530

    Add files via upload

---
+++ react-js/module-3/root-var-app/src/components/Calc.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Calc() {
+  return (
+    <div>
+      <div className='Calculator'>
+      <h1>Calculate anything Else here</h1>
+      </div>
+    </div>
+  )
+}
