7ddf5cca95cbce0c82aa747572e2f1a1ffc406a8
Rutu <123353444+rutudobariya@users.noreply.github.com>
2024-03-18 10:47:31 +0530

    Add files via upload

---
+++ react-js/module-3/react-keys-function/src/components/EmployeeList.js
@@ -0,0 +1,57 @@
+import React from "react";
+// react-keys : react-key are used to key are work as attributes and is used to pass data as query string for using to udpdate | edit | delete particulars data passing with its key.
+
+// A class is used to create a member function
+// A class property used by another class using extends it meanse a class property used by another class it meanse inherited by another class.
+
+// react-listing : list any data inside of tables | sections applied loop or map() functions
+const emp=[
+    {
+        id:1001, 
+        name:'Brijesh',
+        age:32 
+    
+    },
+    {
+        id:1002, 
+        name:'Jatin',
+        age:23 
+    
+    },
+    {
+        id:1001, 
+        name:'Fenish',
+        age:25
+    
+    },
+    {
+        id:1001, 
+        name:'Parn',
+        age:20
+    
+    }
+]
+
+
+
+ 
+
+
+export default function EmployeeList() {
+  return (
+    <div>
+    <h1>Hi i am just Load Employee List</h1>
+
+      {emp.map((items)=>
+      {
+      return(
+        <>
+
+        <p key={items.id}><b>Employee name is :</b>{items.name}</p>
+        </>
+      )
+      })
+      }
+    </div>
+  )
+}
